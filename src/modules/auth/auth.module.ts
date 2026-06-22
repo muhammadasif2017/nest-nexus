3729358c8682c80d3a525b208e44ba92b937e1d9
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2026-06-22 15:37:22 +0500

    refactor(auth): remove duplicate GraphQL exposure of auth mutations

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthService } from './auth.service';
-import { AuthResolver } from './auth.resolver';
 import { AuthController } from './auth.controller';
 import { TokenService } from './token.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
@@ -37,7 +36,6 @@ import { QueuesModule } from '../../core/queues/queues.module';
   ],
   providers: [
     AuthService,
-    AuthResolver,
     TokenService,
     TwoFactorService,
     MagicLinkService,
